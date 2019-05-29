687c8e74ed27f4664586b3d9639ff9a0a82ca118
Tino Matsika <43498809+Matsika@users.noreply.github.com>
2019-05-29 16:58:45 -0230

    Update TempAccount.java
    I am using mySQL to create a database that has the user names and their passwords so mySQL should handle password and username verification

--- TempReg/TempAccount.java
+++ TempReg/TempAccount.java
@@ -56,58 +56,7 @@ public class TempAccount {
     	this.email = email;
     }
     
-    public boolean verifyPassword(String password)
-    {
-        int specialCharacters = 0;
-        int digitCount=0;
-        int upperCaseChars = 0;
-        int lowerCaseChars = 0;
-        
-        //password must be at least 8 characters long
-        if(password.length() < 8)
-        {
-            return false;
-        } 
-        else
-        {
-            for(int i=0 ; i < password.length(); i++)  
-            {
-                char c = password.charAt(i);
-                if(Character.isUpperCase(c))
-                {
-                   upperCaseChars++;
-                }
-                if(Character.isLowerCase(c))
-                {    
-                   lowerCaseChars++; 
-                }
-              
-                if(Character.isDigit(c))
-                {
-                    digitCount++;
-                }
-                Pattern p = Pattern.compile("[^A-Za-z0-9]");
-                Matcher m = p.matcher(password);  
-                boolean b = m.find();
-                if(b)
-                {
-                    specialCharacters++;
-                } 
-                
-            } 
-            //for the password to be valid it should have a least one uppercase letter,
-            //one lowercase letter,at least one special character and at least one digit
-            if(upperCaseChars >= 1 && lowerCaseChars >=1 && digitCount >=1 && specialCharacters >=1)
-            {
-                return true;
-            } 
-            else
-            {
-                return false;
-            }     
-        }    
-        
-    }  
+    
     
     
 }
